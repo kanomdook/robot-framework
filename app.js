9dcec39f3d8eb64d741ce44639051c559a1fae89
kanomdook <kanung.2535@gmail.com>
2020-01-16 17:46:22 +0700

    [add] app.js

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log('Running Test Case...');
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+const directory = 'test';
+const cmd = 'robot ' + path.join(directory, 'testCase01.robot');
+
+fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        const spl = file.split('.');
+        if (spl[1] === 'log') {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+            });
+        }
+    }
+});
+
+fs.readdir('./', (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        const spl = file.split('.');
+        if (spl[1] === 'log') {
+            fs.unlink(path.join('./', file), err => {
+                if (err) throw err;
+            });
+        }
+    }
+});
+
+exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
